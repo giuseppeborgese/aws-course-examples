ae6d0dc3a4420310f17f783540715199c4b19a7f
Giuseppe Borgese <giuseppeborgese@gmail.com>
2023-10-04 16:07:28 +0200

    added vpc creation to save time

--- ecs-demo/main.tf
+++ ecs-demo/main.tf
@@ -1,13 +1,33 @@
+locals { 
+  region = "us-east-1"
+}
 provider "aws" {
-  region = "eu-west-1"
+  region = local.region
 }
 
 module "ecs-fargate-poc" {
   source  = "giuseppeborgese/ecs-fargate-poc/aws"
   app_port = 80
-  private_subnets = ["subnet-078171ba6d73111d9","subnet-085f4cc0b227a5d41"]
-  public_subnets =  ["subnet-eca9a0a5", "subnet-2104447a"]
+  private_subnets = module.vpc.private_subnets
+  public_subnets =  module.vpc.public_subnets
   prefix = "terraform-poc"
   image_uri_with_tag = "public.ecr.aws/nginx/nginx:latest"
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "ECS-DEMO-TEST"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
