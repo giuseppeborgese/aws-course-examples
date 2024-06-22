2bdbb2a9f148fb1fc30580153694d5a52372874e
Giuseppe Borgese <giuseppeborgese@gmail.com>
2024-06-22 07:37:28 +0200

    aaaaa

--- codewhispered/test.py
+++ codewhispered/test.py
@@ -8,5 +8,10 @@ for instance in ec2.instances.all():
     print(instance.public_ip_address)
     print(instance.tags)
 
-
+# list all my s3 buckets
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket.name)
+    print(bucket.creation_date)
+    print(bucket.tags)
     
